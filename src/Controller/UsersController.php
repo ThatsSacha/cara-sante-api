b4d541dfddfc5b8ccab823cd9e72746eb4bdd89a
Sacha COHEN <contact@sacha-cohen.fr>
2021-08-04 05:15:11 +0200

    Role hierarchy & remove other request

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -3,12 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Users;
-use App\Form\UsersType;
 use App\Service\UsersService;
 use App\Repository\UsersRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -21,14 +19,6 @@ class UsersController extends AbstractController
     public function __construct(UsersService $usersService)
     {
         $this->usersService = $usersService;
-    }
-
-    #[Route('/', name: '', methods: ['GET'])]
-    public function index(UsersRepository $usersRepository): Response
-    {
-        return $this->render('users/index.html.twig', [
-            'users' => $usersRepository->findAll(),
-        ]);
     }
 
     #[Route('', name: 'users_new', methods: ['OPTIONS', 'POST'])]
@@ -48,43 +38,5 @@ class UsersController extends AbstractController
         }
 
         return new JsonResponse($create, $status);
-    }
-
-    #[Route('/{id}', name: 'users_show', methods: ['GET'])]
-    public function show(Users $user): Response
-    {
-        return $this->render('users/show.html.twig', [
-            'user' => $user,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'users_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Users $user): Response
-    {
-        $form = $this->createForm(UsersType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('users_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('users/edit.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'users_delete', methods: ['POST'])]
-    public function delete(Request $request, Users $user): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($user);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('users_index', [], Response::HTTP_SEE_OTHER);
     }
 }
