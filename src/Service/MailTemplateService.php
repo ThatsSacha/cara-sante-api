ca1de5ed95eab24ec18225d2d76ea53538adc89d
Sacha COHEN <contact@sacha-cohen.fr>
2021-09-30 11:20:08 +0200

    Fix forgot template last sentence

--- src/Service/MailTemplateService.php
+++ src/Service/MailTemplateService.php
@@ -2019,7 +2019,7 @@ a.es-button, button.es-button {
                     <div class="esd-delete" title="Delete">
                         <a><span class="es-icon-delete"></span></a>
                     </div>
-                </div><p style="color: #777777; font-size: 12px;">Vous recevez ce mail car un administrateur de l\'application '. $_ENV['FRONT_URL'] .' a renseigné votre adresse à la création d\'un nouvel utilisateur.</p></td> 
+                </div><p style="color: #777777; font-size: 12px;">Vous recevez ce mail car une demande de rénitialisation de mot de passe pour votre compte a été soumise via '. $_ENV['FRONT_URL'] .'.</p></td> 
                      </tr> 
                     </tbody> 
                    </table></td> 
