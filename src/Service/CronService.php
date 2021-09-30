5a478de048b91b70ba802fb12d854c6deb985ea9
Sacha COHEN <contact@sacha-cohen.fr>
2021-09-30 19:26:29 +0200

    Remove extends cron service

--- src/Service/CronService.php
+++ src/Service/CronService.php
@@ -2,7 +2,7 @@
 
 namespace App\Service;
 
-class CronService extends AbstractRestService {
+class CronService {
     public function saveDatabase(string $token) {
         shell_exec('/bin/mysqldump --host='. $_ENV['DB_HOST'] .' --user='. $_ENV['DB_USER'] .' --password='. $_ENV['DB_PASSWORD'] .' '. $_ENV['DB_NAME'] .' > savedb.sql');
     }
