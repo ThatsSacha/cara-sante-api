e0b209b762bbfd201ef450076288bd0df008cd25
Sacha COHEN <contact@sacha-cohen.fr>
2021-10-13 17:36:03 +0200

    Search create history even if not result

--- src/Service/SearchService.php
+++ src/Service/SearchService.php
@@ -35,21 +35,19 @@ class SearchService extends AbstractRestService {
         $resp = array('status' => 200, 'results' => []);
 
         if (count($patients) > 0) {
-            $search = [];
-
             foreach($patients as $patient) {
                 $patient['id'] = (int) $patient['id'];
                 $patientSerialized = $this->serializer->deserialize(json_encode($patient), 'App\Entity\Patient', 'json');
                 
                 array_push($resp['results'], $patientSerialized->jsonSerialize());
-
-                $search['searchedBy'] = $user->getId();
-                $search['searchedAt'] = date_format(date_create(), 'Y-m-d H:i:s');
-                $search['subject'] = $data['search'];
             }
-
-            $this->create($search);
         }
+
+        $search = [];
+        $search['searchedBy'] = $user->getId();
+        $search['searchedAt'] = date_format(date_create(), 'Y-m-d H:i:s');
+        $search['subject'] = $data['search'];
+        $this->create($search);
 
         return $resp;
     }
